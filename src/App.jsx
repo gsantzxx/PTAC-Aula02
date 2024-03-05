1c072ae24a653744973ade17a61e442c10d9a2ab
gsantzxx <luis.oliveira11@estudante.ifms.edu.br>
2024-03-05 10:13:55 -0400

    Criação do Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 export default function App() {
 
   return(
     <main>
         <Header/>
+        <Footer/>
       </main>
  
   );

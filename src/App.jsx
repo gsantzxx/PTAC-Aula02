662f8f23ff0a136073e4747aee4009655241eda7
gsantzxx <luis.oliveira11@estudante.ifms.edu.br>
2024-03-19 09:24:06 -0400

    botão

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 import "./globals.css"
 export default function App() {
 
   return(
     <main>
         <Header/>
+        <Main/>
         <Footer/>
       </main>
  
